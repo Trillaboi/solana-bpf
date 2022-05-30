e0fea5d51dcad7dccfdf0f3234f1a2a194273efc
Chisom Ikejiani <trillaboi@gmail.com>
2022-05-30 01:26:30 -0400

    done

--- src/error.rs
+++ src/error.rs
@@ -12,6 +12,12 @@ pub enum EscrowError {
     /// Not Rent Exempt
     #[error("Not Rent Exempt")]
     NotRentExempt,
+    /// Expected Amount Mismatch
+    #[error("Expected Error Mismatch")]
+    ExpectedAmountMismatch,
+    /// Amount Overflow
+    #[error("Amount Overflow")]
+    AmountOverflow,
 }
 
 impl From<EscrowError> for ProgramError {
